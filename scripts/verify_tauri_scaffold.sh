de63a09121eba90fa41930ec6397d826ba8fcbdf
MJY <jmoon0227@gmail.com>
2026-05-24 16:48:23 +0900

    Sync selected conversation after export

--- scripts/verify_tauri_scaffold.sh
+++ scripts/verify_tauri_scaffold.sh
@@ -152,6 +152,7 @@ require_contains "$APP_DIR/src/main.js" 'invoke("production_message_received_exp
 require_contains "$APP_DIR/src/main.js" 'invoke("production_message_transcript_export"'
 require_contains "$APP_DIR/src/main.js" 'loadProductionTwoProfileTranscript'
 require_contains "$APP_DIR/src/main.js" 'refreshTwoProfileConversationAfterManualImport'
+require_contains "$APP_DIR/src/main.js" 'syncTwoProfileConversationAfterManualExport'
 require_contains "$APP_DIR/src/main.js" 'replyToLatestTwoProfileMessage'
 require_contains "$APP_DIR/src/main.js" 'selectTwoProfileConversationEntryForReview'
 require_contains "$APP_DIR/src/main.js" 'selectReplyAfterDeliveredReview'
