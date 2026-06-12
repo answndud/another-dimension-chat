f03879462234928a69a95a138e82525b98bc19f8
MJY <jmoon0227@gmail.com>
2026-06-12 23:02:05 +0900

    Align mobile shell presentation labels

--- scripts/verify_ios_shell_boundary.sh
+++ scripts/verify_ios_shell_boundary.sh
@@ -108,6 +108,9 @@ require_text "apps/mobile/ios/AnotherDimension/ContentView.swift" "sharedCore.pr
 require_text "apps/mobile/ios/AnotherDimension/ContentView.swift" "sharedCore.inviteCodeCreateJoin"
 require_text "apps/mobile/ios/AnotherDimension/ContentView.swift" "sharedCore.manualEnvelopeExportImport"
 require_text "apps/mobile/ios/AnotherDimension/ContentView.swift" "sharedCore.redactedSupportDiagnostics"
+require_text "apps/mobile/ios/AnotherDimension/ContentView.swift" "failure_class="
+require_text "apps/mobile/ios/AnotherDimension/ContentView.swift" "recovery_next_action="
+require_text "apps/mobile/ios/AnotherDimension/ContentView.swift" "public_non_claims="
 
 require_text "apps/mobile/ffi/README.md" "production_mobile_shared_core_api_freeze_boundary_summary"
 require_text "apps/mobile/ffi/shared_core_mobile_api_contract.json" '"first_binding_unit": "status_and_redacted_diagnostics_read_only_adapter"'
