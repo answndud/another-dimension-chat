69f851c7de9295df5ef4b015aa0165027bf9f73d
MJY <jmoon0227@gmail.com>
2026-07-13 10:37:55 +0900

    Point downloads to bootstrap fix release

--- apps/desktop-tauri/src/main.js
+++ apps/desktop-tauri/src/main.js
@@ -3908,7 +3908,7 @@ function renderVersionIntegrityStatus(input = {}) {
     currentVersion: FIELD_TEST_APP_VERSION,
     buildChannel: FIELD_TEST_BUILD_CHANNEL,
     buildCommit: FIELD_TEST_BUILD_COMMIT,
-    currentReleaseTag: "v0.1.0-ui-fix",
+    currentReleaseTag: "v0.1.0-bootstrap-fix",
     ...input,
   });
   setText(fields.versionIntegrityStatus, view.summary);
