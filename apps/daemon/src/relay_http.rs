c9416ebcb19c1d3025519558fb08604e0484551b
MJY <jmoon0227@gmail.com>
2026-08-05 13:10:00 +0900

    feat(transport): connect pinned HTTPS relay client

--- apps/daemon/src/relay_http.rs
+++ apps/daemon/src/relay_http.rs
@@ -8,24 +8,104 @@
 use crate::delivery::RelayEnvelope;
 use crate::trust::TlsCertificatePin;
 use axum::http::Uri;
+use rustls::{
+    client::WebPkiServerVerifier,
+    pki_types::{CertificateDer, ServerName, UnixTime},
+    ClientConfig, RootCertStore,
+};
 use serde::Deserialize;
+use sha2::{Digest, Sha256};
 use std::{
     io::{Read, Write},
     net::TcpStream as StdTcpStream,
+    sync::Arc,
     time::Duration,
 };
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
     net::TcpStream,
     time::timeout,
 };
+use tokio_rustls::TlsConnector;
+use webpki_roots::TLS_SERVER_ROOTS;
 
 const MAX_RESPONSE_BYTES: usize = 128 * 1024;
+
+#[derive(Debug)]
+struct PinnedServerCertVerifier {
+    inner: Arc<WebPkiServerVerifier>,
+    pin: TlsCertificatePin,
+}
+
+impl rustls::client::danger::ServerCertVerifier for PinnedServerCertVerifier {
+    fn verify_server_cert(
+        &self,
+        end_entity: &CertificateDer<'_>,
+        intermediates: &[CertificateDer<'_>],
+        server_name: &ServerName<'_>,
+        ocsp_response: &[u8],
+        now: UnixTime,
+    ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
+        let verified = self.inner.verify_server_cert(
+            end_entity,
+            intermediates,
+            server_name,
+            ocsp_response,
+            now,
+        )?;
+        let digest = Sha256::digest(end_entity.as_ref());
+        if digest.as_slice() != self.pin.as_bytes() {
+            return Err(rustls::Error::General(
+                "relay certificate pin mismatch".into(),
+            ));
+        }
+        Ok(verified)
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        message: &[u8],
+        cert: &CertificateDer<'_>,
+        dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        self.inner.verify_tls12_signature(message, cert, dss)
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        message: &[u8],
+        cert: &CertificateDer<'_>,
+        dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        self.inner.verify_tls13_signature(message, cert, dss)
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+        self.inner.supported_verify_schemes()
+    }
+}
+
+fn tls_config(pin: TlsCertificatePin) -> Result<Arc<ClientConfig>, RelayError> {
+    let mut roots = RootCertStore::empty();
+    roots.extend(TLS_SERVER_ROOTS.iter().cloned());
+    let inner = WebPkiServerVerifier::builder(Arc::new(roots))
+        .build()
+        .map_err(|_| RelayError::TlsConfiguration)?;
+    let verifier = Arc::new(PinnedServerCertVerifier { inner, pin });
+    Ok(Arc::new(
+        ClientConfig::builder()
+            .dangerous()
+            .with_custom_certificate_verifier(verifier)
+            .with_no_client_auth(),
+    ))
+}
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum RelayError {
     InvalidEndpoint,
     TrustRequired,
+    TlsConfiguration,
+    TlsHandshake,
     UnsupportedTransport,
     InvalidCapability,
     Connect,
@@ -41,6 +121,7 @@ pub struct RelayEndpoint {
     pub path: String,
     pub capability: String,
     pub tls_certificate_pin: Option<TlsCertificatePin>,
+    pub tls_server_name: String,
 }
 
 impl RelayEndpoint {
@@ -88,6 +169,7 @@ impl RelayEndpoint {
             path: uri.path().to_owned(),
             capability: capability.to_owned(),
             tls_certificate_pin,
+            tls_server_name: host.to_owned(),
         })
     }
 
@@ -258,15 +340,38 @@ impl RelayClient {
         body: &[u8],
     ) -> Result<(u16, Vec<u8>), RelayError> {
         let address = self.endpoint.address()?;
-        let stream = timeout(self.request_timeout, TcpStream::connect(address))
+        let tcp = timeout(self.request_timeout, TcpStream::connect(address))
             .await
             .map_err(|_| RelayError::Timeout)?
             .map_err(|_| RelayError::Connect)?;
-        let mut stream = stream;
+        if let Some(pin) = self.endpoint.tls_certificate_pin {
+            let config = tls_config(pin)?;
+            let server_name = ServerName::try_from(self.endpoint.tls_server_name.clone())
+                .map_err(|_| RelayError::TlsConfiguration)?;
+            let stream = TlsConnector::from(config)
+                .connect(server_name, tcp)
+                .await
+                .map_err(|_| RelayError::TlsHandshake)?;
+            return self.request_async_stream(stream, method, path, body).await;
+        }
+        self.request_async_stream(tcp, method, path, body).await
+    }
+
+    async fn request_async_stream<S>(
+        &self,
+        mut stream: S,
+        method: &str,
+        path: &str,
+        body: &[u8],
+    ) -> Result<(u16, Vec<u8>), RelayError>
+    where
+        S: AsyncRead + AsyncWrite + Unpin,
+    {
         let authority = self
             .endpoint
             .origin
-            .strip_prefix("http://")
+            .split_once("://")
+            .map(|(_, authority)| authority)
             .ok_or(RelayError::InvalidEndpoint)?;
         let request = format!(
             "{method} {path} HTTP/1.1\r\nHost: {authority}\r\nConnection: close\r\nAccept: application/json\r\nContent-Type: application/json\r\nX-Ad-Relay-Capability: {}\r\nContent-Length: {}\r\n\r\n",
@@ -298,17 +403,38 @@ impl RelayClient {
         body: &[u8],
     ) -> Result<(u16, Vec<u8>), RelayError> {
         let address = self.endpoint.address()?;
-        let mut stream = StdTcpStream::connect(address).map_err(|_| RelayError::Connect)?;
-        stream
-            .set_read_timeout(Some(self.request_timeout))
+        let tcp = StdTcpStream::connect(address).map_err(|_| RelayError::Connect)?;
+        tcp.set_read_timeout(Some(self.request_timeout))
             .map_err(|_| RelayError::Connect)?;
-        stream
-            .set_write_timeout(Some(self.request_timeout))
+        tcp.set_write_timeout(Some(self.request_timeout))
             .map_err(|_| RelayError::Connect)?;
+        if let Some(pin) = self.endpoint.tls_certificate_pin {
+            let config = tls_config(pin)?;
+            let server_name = ServerName::try_from(self.endpoint.tls_server_name.clone())
+                .map_err(|_| RelayError::TlsConfiguration)?;
+            let connection = rustls::ClientConnection::new(config, server_name)
+                .map_err(|_| RelayError::TlsHandshake)?;
+            let mut stream = rustls::StreamOwned::new(connection, tcp);
+            return self.request_blocking_stream(&mut stream, method, path, body);
+        }
+        self.request_blocking_stream(tcp, method, path, body)
+    }
+
+    fn request_blocking_stream<S>(
+        &self,
+        mut stream: S,
+        method: &str,
+        path: &str,
+        body: &[u8],
+    ) -> Result<(u16, Vec<u8>), RelayError>
+    where
+        S: Read + Write,
+    {
         let authority = self
             .endpoint
             .origin
-            .strip_prefix("http://")
+            .split_once("://")
+            .map(|(_, authority)| authority)
             .ok_or(RelayError::InvalidEndpoint)?;
         let request = format!(
             "{method} {path} HTTP/1.1\r\nHost: {authority}\r\nConnection: close\r\nAccept: application/json\r\nContent-Type: application/json\r\nX-Ad-Relay-Capability: {}\r\nContent-Length: {}\r\n\r\n",
